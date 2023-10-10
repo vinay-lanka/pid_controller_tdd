edb4ef2ba75c6ab3dadd5c2a6577dc4f8502da81
mvboiii <113264700+mvboiii@users.noreply.github.com>
2023-10-10 00:51:25 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file main.cpp
+ * @author Vinay Lanka (vlanka@umd.edu)
+ * @brief Driver program for the PID Controller implementation
+ * @version 0.1
+ * @date 2023-10-04
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "pid_lib.hpp"
+#include <cstdio>
+
+int main() {
+
+    PID pid = PID(0.1, 100, -100, 0.1, 0.01, 0.5);
+
+    double val = 20;
+    for (int i = 0; i < 100; i++) {
+        double inc = pid.calculate(0, val);
+        printf("val:% 7.3f inc:% 7.3f\n", val, inc);
+        val += inc;
+    }
+
+    return 0;
+}
