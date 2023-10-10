1657ca4a747ac4b92e3315a546ab8aeaaa52d5b9
mvboiii <113264700+mvboiii@users.noreply.github.com>
2023-10-10 01:47:19 -0400

    Update src.cpp
    adding class implementations so that the tests pass

--- libs/pid_lib/src.cpp
+++ libs/pid_lib/src.cpp
@@ -1,6 +1,7 @@
 /**
  * @file src.cpp
  * @author Vinay Lanka (vlanka@umd.edu)
+ * @author Manav Nagda (manav19@umd.edu)
  * @brief Source folder for the implementation of the PID Controller
  * @version 0.1
  * @date 2023-10-08
@@ -20,7 +21,17 @@
  * @param Kd
  * @param Ki
  */
-PID::PID(double dt, double max, double min, double Kp, double Kd, double Ki) {}
+PID::PID ( double dt, double max, double min, double Kp, double Kd, double Ki ) :
+    _dt(dt),
+    _max(max),
+    _min(min),
+    _Kp(Kp),
+    _Kd(Kd),
+    _Ki(Ki),
+    _pre_error(0),
+    _integral(0)
+{
+}
 
 /**
  * @brief The PID Calculate stub.
@@ -29,4 +40,32 @@ PID::PID(double dt, double max, double min, double Kp, double Kd, double Ki) {}
  * @param pv
  * @return double
  */
-double PID::calculate(double setpoint, double pv) { return -1; }
+double PID::calculate(double setpoint, double pv) { 
+    // Calculate error
+    double error = setpoint - pv;
+
+    // Proportional term
+    double Pout = _Kp * error;
+
+    // Integral term
+    _integral += error * _dt;
+    double Iout = _Ki * _integral;
+
+    // Derivative term
+    double derivative = (error - _pre_error) / _dt;
+    double Dout = _Kd * derivative;
+
+    // Calculate total output
+    double output = Pout + Iout + Dout;
+
+    // Restrict to max/min
+    if( output > _max )
+        output = _max;
+    else if( output < _min )
+        output = _min;
+
+    // Save error to previous error
+    _pre_error = error;
+
+    return output;
+}
